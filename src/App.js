78fefc9e2eeb2a32e635300563327082a5ccb6aa
Zuhri Nurhuda <zuhri.nurhuda@gmail.com>
2018-02-21 20:28:07 +0700

    setup router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react'
-import logo from './assets/img/logo.svg'
-import './assets/css/App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import { Home } from './components'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
     )
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Container } from 'semantic-ui-react'
+
+class Home extends Component {
+  render () {
+    return (
+      <Container></Container>
+    )
+  }
+}
+
+export default Home
